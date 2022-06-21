04e798b1e21b5793af4768530a708b1db54b71fa
Mr Muscl <mrmuscl@mail.ru>
2022-06-21 19:57:18 +0300

    Added execute test view

--- src/Examination.WEB/Controllers/StudentTestController.cs
+++ src/Examination.WEB/Controllers/StudentTestController.cs
@@ -26,5 +26,48 @@ namespace Examination.WEB.Controllers
             var model = _db.GetTests();
             return View(model);
         }
+
+        [HttpGet]
+        public IActionResult Execute(int id, int? questionNumber) 
+        {
+            Question question;
+            
+            var test = _db.GetTestWithQuestionsAndAnswers(id);
+            ViewBag.QuestionNumber = questionNumber;
+
+            if (questionNumber == null)
+            {
+                question = test.Questions.FirstOrDefault();                
+            }
+            else
+            {
+                question = test.Questions.Where(q => q.Number == questionNumber).FirstOrDefault();
+            }
+
+            //if (question == null)
+            //    return NotFoundResult
+
+            return View(question);
+        }
+
+        [HttpPost]
+        public IActionResult ExecutePrev(int id, int? questionNumber)
+        {
+            var question = _db.GetQuestion(id);
+            ViewBag.QuestionNumber = questionNumber;
+
+            
+            
+            return RedirectToAction("Execute", "StudentTest", new { Id = 1, QuestinNumber = questionNumber - 1});
+        }
+
+        [HttpPost]
+        public IActionResult ExecuteNext(int id, int? questionNumber)
+        {
+            var question = _db.GetQuestion(id);
+            ViewBag.QuestionNumber = questionNumber;
+
+            return RedirectToAction("Execute", "StudentTest", new { Id = 1, QuestinNumber = questionNumber + 1 });
+        }
     }
 }
